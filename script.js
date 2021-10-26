ce23a0dd116783ab321af6cc07100e2e968b5645
Detlef Dev <detlef.visser@gmail.com>
2021-10-26 21:47:43 +0200

    Added dark/light mode toggler and color changer

--- script.js
+++ script.js
@@ -18,4 +18,53 @@ $(document).ready(function(){
       scrollTop : $($(this).attr('href')).offset().top,
     }, 500, 'linear');
   });
+  var colors = ["#33CCFF", "#4933FF", "#F833FF", "#FF3357", "#FFBD33", "#92FF33", "#33FF83"];
+  var i = 0;
+  $(".color").click(function() {
+    if (i < colors.length) {
+    $(":root").css("--accent", colors[i]);
+      i++;
+        if (i == 7) {
+          i = 0;
+        }
+    }
+  });
 });
+
+const themeBtn = document.querySelector('.theme');
+
+function getCurrentTheme(){
+  let theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  localStorage.getItem('mode') ? theme = localStorage.getItem('mode') : null;
+  return theme;
+}
+
+function loadTheme(theme){
+  const root = document.querySelector(':root');
+  if(theme === "light"){
+    themeBtn.innerHTML = `<i class="fas fa-sun"></i>`;
+  } else {
+    themeBtn.innerHTML = `<i class="fas fa-moon"></i>`;
+  }
+  root.setAttribute('color-scheme', `${theme}`);
+}
+
+themeBtn.addEventListener('click', () => {
+  let theme = getCurrentTheme();
+  let audio;
+  if(theme === 'dark'){
+    audio = document.querySelector('.theme-audio--light-on');
+    theme = 'light';
+  } else {
+    audio = document.querySelector('.theme-audio--light-off');
+    theme = 'dark';
+  }
+  audio.currentTime = 0;
+  audio.play();
+  localStorage.setItem('mode', `${theme}`);
+  loadTheme(theme);
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+  loadTheme(getCurrentTheme());
+})
